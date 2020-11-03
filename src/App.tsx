a71aad3ff25e1d07282a9b4b89728d98a21f4bbe
Joseph Daniel Winters <jdwinters96@gmail.com>
2020-11-02 22:19:23 -0500

    Add Windows/UNIX path converter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { MusicRecommender } from './MusicRecommender';
 import { CoronavirusUpdate } from './CoronavirusUpdate';
+import { FilePathConverter } from './FilePathConverter';
 
 function App(): JSX.Element {
   return (
     <div className="app">
       <MusicRecommender />
+      <FilePathConverter />
       <CoronavirusUpdate />
     </div>
   );
---
+++ src/FilePathConverter.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { Widget } from './Widget';
+
+export function FilePathConverter(): JSX.Element {
+  const [unixPath, setUnixPath] = useState('');
+  const [windowsPath, setWindowsPath] = useState('');
+  const windowsCDrive = 'C:';
+  const unixCDrive = '/c';
+  const inputSize = 50;
+
+  const unixToWindows = (path: string): string => {
+    let ret = path.slice(0, 2) === unixCDrive ? windowsCDrive + path.slice(2) : path;
+    ret = ret.replace(/\//gu, '\\');
+    return ret;
+  };
+
+  const windowstoUnix = (path: string): string => {
+    let ret = path.slice(0, 2) === windowsCDrive ? unixCDrive + path.slice(2) : path;
+    ret = ret.replace(/\\/gu, '/');
+    return ret;
+  };
+
+  const handleUnixChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setUnixPath(e.currentTarget.value);
+    setWindowsPath(unixToWindows(e.currentTarget.value));
+  };
+
+  const handleWindowsChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setWindowsPath(e.currentTarget.value);
+    setUnixPath(windowstoUnix(e.currentTarget.value));
+  };
+
+  return (
+    <Widget className="file-path-converter" title="File Path Converter">
+      <p>
+        <input type="text" id="unix-path-input" value={unixPath} size={inputSize} onChange={handleUnixChange} />
+      </p>
+      <p>
+        <input
+          type="text"
+          id="windows-path-input"
+          value={windowsPath}
+          size={inputSize}
+          onChange={handleWindowsChange}
+        />
+      </p>
+    </Widget>
+  );
+}
