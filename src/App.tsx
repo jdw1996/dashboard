24c02a84aca7c80089cc0ab18f3c3f936f3b6178
Joseph Daniel Winters <jdwinters96@gmail.com>
2020-11-02 20:20:26 -0500

    Add CoronavirusUpdate widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { MusicRecommender } from './MusicRecommender';
+import { CoronavirusUpdate } from './CoronavirusUpdate';
 
 function App(): JSX.Element {
   return (
     <div className="app">
       <MusicRecommender />
+      <CoronavirusUpdate />
     </div>
   );
 }
