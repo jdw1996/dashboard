0e55f2a0a2ba87219671fdeb0889d88d2a04af65
Joseph Daniel Winters <jdwinters96@gmail.com>
2022-06-22 13:14:51 -0400

    fix: Remove unnecessary blank line

--- src/SportsSchedule.tsx
+++ src/SportsSchedule.tsx
@@ -4,14 +4,11 @@ import { Widget } from './Widget';
 export function SportsSchedule(): JSX.Element {
   return (
     <Widget className="sports-schedule" title="Sports Schedule">
-      <p>
-        <a href="https://josephwinters.com/sports-schedule/?MLB_TOR=Royal&NHL_TOR=Navy&NBA_TOR=Purple&MLB_NYM=Orange&NFL_BUF=Red">
-          Jays, Leafs, Raptors, Mets, Bills
-        </a>
-      </p>
-      <p>
-        <a href="https://motorsport-events.netlify.app/">Upcoming motorsports events</a>
-      </p>
+      <a href="https://josephwinters.com/sports-schedule/?MLB_TOR=Royal&NHL_TOR=Navy&NBA_TOR=Purple&MLB_NYM=Orange&NFL_BUF=Red">
+        Jays, Leafs, Raptors, Mets, Bills
+      </a>
+      <br />
+      <a href="https://motorsport-events.netlify.app/">Upcoming motorsports events</a>
     </Widget>
   );
 }
